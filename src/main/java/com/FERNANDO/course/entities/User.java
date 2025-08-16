1364d9162fb65a8e607c7595e25a17dc158a8916
Fernando Freitas <fernandofreitas571@gmail.com>
2025-08-16 10:26:22 -0300

    H2 database, test profile, JPA

--- src/main/java/com/FERNANDO/course/entities/User.java
+++ src/main/java/com/FERNANDO/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.FERNANDO.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;  // O valor pode ser qualquer número, mas deve ser único
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
